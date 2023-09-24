fa4c5319d3f6857eab0f39e48feca489b9c7e9bf
Collin Guenther <cguen@unc.edu>
2023-09-24 16:39:10 -0400

    workin 2

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,6 +1,11 @@
 """Abstracting Wordle with Functions."""
 ___author___ = "730663338"
 
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
 def contains_char(str1: str, str2: str) -> bool:
     assert len(str2) == 1
     i = 0
@@ -14,5 +19,16 @@ def contains_char(str1: str, str2: str) -> bool:
     else:
         return False
 
+def emojified(secret: str, guess: str) -> str:
+    assert len(secret) == len(guess)
+    boxes = ""
+    i = 0
+    while (1 < len(secret)):
+        if secret[i] == guess [i]:
+            boxes += GREEN_BOX
+        elif contains_char(secret, guess[i]) is True:
+            boxes += YELLOW_BOX
+        else:
+            boxes += WHITE_BOX
+    return boxes
 
-    
