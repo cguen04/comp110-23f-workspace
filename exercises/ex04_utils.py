b1a9ba8f4a5a16f20b56f154b2de40476188c26d
Collin Guenther <cguen@unc.edu>
2023-10-11 21:49:34 -0400

    workin

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,42 @@
+"""List utility functions."""
+__author__ = "730663338"
+
+
+def all(input: list[int], x: int) -> bool:
+    """Returns True if all ints in the list are the same."""
+    if len(input) <= 0:
+        return(print(False))
+    i = 0
+    while len(input) > i:
+        if input[i] == x:
+            i += 1
+        else:
+            return(print(False))
+    return(print(True))
+
+
+def max(input: list[int]) -> int:
+    """Returns the highest value in the list."""
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty list")
+    if len(input) == 1:
+        return(print(input[0])) 
+    i = 1
+    x = input[0]
+    while len(input) > i:
+        if x < input[i]:
+            x = input[i]
+        i += 1
+    return(print(x))
+
+
+def is_equal(input1: list[int], input2: list[int]) -> bool:
+    """Returns True if both values at each index in both lists are equal."""
+    if len(input1) != len(input2):
+        return(print(False))
+    i = 0
+    while (len(input1) and len(input2)) > i:
+        if input1[i] != input2[i]:
+            return(print(False))
+        i += 1
+    return(print(True))
