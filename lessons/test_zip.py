f955b3d3a3bcbe76044e2c7eac4b1126dc53a089
Collin Guenther <cguen@unc.edu>
2023-10-27 23:31:52 -0400

    work

--- lessons/test_zip.py
+++ lessons/test_zip.py
@@ -4,18 +4,19 @@ ___author___ = "730663338"
 
 from lessons.zip import zip
 
+
 def test_zip_len() -> None:
-    """zip with two lists of unequal length should return an empty list."""
+    """Function zip with two lists of unequal length should return an empty list."""
     assert zip(["hello", "world"], [1]) == {}
 
 
 def test_1_zip() -> None:
-    """zip(["hello"], [1]) == {"hello": 1}"""
+    """Function zip(["hello"], [1]) == {"hello": 1}."""
     assert zip(["hello"], [1]) == {"hello": 1}
 
 
 def test_zip_lists() -> None:
-    """zip(["a", "b", "c", "d"], [1, 2, 3, 4] == {"a": 1, "b": 2, "c": 3, "d": 4})"""
+    """Function zip(["a", "b", "c", "d"], [1, 2, 3, 4] == {"a": 1, "b": 2, "c": 3, "d": 4})."""
     list1 = ["a", "b", "c", "d"]
     list2 = [1, 2, 3, 4]
     assert zip(list1, list2) == {"a": 1, "b": 2, "c": 3, "d": 4}
