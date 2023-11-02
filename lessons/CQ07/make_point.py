00901db460fd1e02b9e9fe35fc6c017583c8d344
Collin <cguen@unc.edu>
2023-11-02 12:07:12 -0400

    wo

--- lessons/CQ07/make_point.py
+++ lessons/CQ07/make_point.py
@@ -2,6 +2,6 @@ from lessons.CQ07.point import Point
 my_point: Point = Point(20, 20)
 my_point.scale_by(2)
 second_point: Point = my_point.scale(3)
-print(my_point)
-print(second_point)
+print(my_point.x)
+print(second_point.x)
 
