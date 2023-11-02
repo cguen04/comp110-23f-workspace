1bb557c982ac6bafc91b28d973fb23cfe764bfc9
Collin Guenther <cguen@unc.edu>
2023-11-01 22:45:07 -0400

    done...hopefully

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -3,6 +3,7 @@ __author__ = "730663338"
 
 
 def invert(dict1: dict[str, str]) -> dict[str, str]:
+    """Inverts the keys and values of a dict."""
     dict2: dict[str, str] = {}
     new_keys: list[str] = []
     for key in dict1:
@@ -21,6 +22,7 @@ def invert(dict1: dict[str, str]) -> dict[str, str]:
 
 
 def favorite_color(colors:dict[str, str]) -> str:
+    """Returns the color most often said to be the favorite."""
     color_dict_vals: dict[str, int] = {}
     color_list: list[str] = []
     for name in colors:
@@ -43,6 +45,7 @@ def favorite_color(colors:dict[str, str]) -> str:
 
 
 def count(list_in: list[str]) -> dict[str, int]:
+    """Counts how many times an item appears in a list and returns a dict with the item as the key and the frequency as the value."""
     dict_out: dict[str, int] = {}
     for val in list_in:
         if val in dict_out:
@@ -51,5 +54,25 @@ def count(list_in: list[str]) -> dict[str, int]:
             dict_out[val] = 1
     return dict_out
 
+
 def alphabetizer(list_in: list[str]) -> dict[str, list[str]]:
-    
+    """Takes a list of items and returns a dictionary with the items sorted by the first character"""
+    dict_out: dict[str, list[str]] = {}
+    for elem in list_in:
+        x = ""
+        x = elem[0].lower()
+        dict_out[x] = []
+    for key in dict_out:
+        for elem in list_in:
+            if key == elem[0].lower():
+                dict_out[key] += [elem]
+    return dict_out
+
+
+def update_attendance(dict_in: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
+    """Takes an existing dictionary and updates it with new values."""
+    if day in dict_in:
+        dict_in[day] += [student]
+    if day not in dict_in:
+        dict_in[day] = [student]
+    return dict_in
