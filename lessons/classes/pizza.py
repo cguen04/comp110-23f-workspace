7dace6353a0e1f24ce07657f8d583d87a1082e8c
Collin <cguen@unc.edu>
2023-11-02 11:17:34 -0400

    work

---
+++ lessons/classes/pizza.py
@@ -0,0 +1,20 @@
+"""Defining a class."""
+
+
+"""
+Think of a class definition as a "roadmap"
+for objects that belong 
+to the class
+You are defining the underlying structure
+every instance of this class will have
+"""
+
+class Pizza:
+    """This is my class to represent pizza."""
+
+    # attributes
+    # syntax <name> : <type> 
+    size: str
+    toppings: int
+    gluten_free: bool
+    
---
+++ lessons/classes/pizza_orders.py
@@ -0,0 +1,24 @@
+"""Instantiating a class."""
+
+"""
+This is where we instantiate the class 
+we defined in classes.py.
+In other words, we're creating objects 
+that belong to that class.
+"""
+
+# import the class
+# from <file_name>.<module_name> import <class>
+from lessons.classes.pizza import Pizza
+
+
+my_pizza: Pizza = Pizza() # constructor
+my_pizza.size = "large"
+my_pizza.toppings = 10
+my_pizza.gluten_free = True
+
+print("Size: ")
+print(my_pizza.size)
+
+print("my_pizza: ")
+print(my_pizza)
