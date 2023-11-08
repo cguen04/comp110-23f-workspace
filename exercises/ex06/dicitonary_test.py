81b6f3fbc9f0600f3f581f5e32fd39f1f38f36ae
Collin Guenther <cguen@unc.edu>
2023-11-08 18:24:24 -0500

    work

--- exercises/ex06/dicitonary_test.py
+++ exercises/ex06/dicitonary_test.py
@@ -13,3 +13,13 @@ def test_invert_use_1() -> None:
     """Should return {"world": "hello", "meatballs": "spaghetti"}"""
     assert invert({"hello": "world", "spaghetti": "meatballs"}) == {"world": "hello", "meatballs": "spaghetti"}
 
+
+def test_invert_use_2() -> None:
+    """Should return {"b": "a", "d": "c", "f": "e", "h": "g", "j": "i", "l": "k"}"""
+    assert invert({"a": "b", "c": "d", "e": "f", "g": "h", "i": "j", "k": "l"}) == {"b": "a", "d": "c", "f": "e", "h": "g", "j": "i", "l": "k"}
+
+
+def test_invert_edge() -> None:
+    """Should return {}"""
+    assert invert() == {}
+
