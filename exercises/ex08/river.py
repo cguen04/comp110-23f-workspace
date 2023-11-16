a8410d3b6289bb6b650623e943668278160d8c15
Collin Guenther <cguen@unc.edu>
2023-11-15 20:42:41 -0500

    woe

--- exercises/ex08/river.py
+++ exercises/ex08/river.py
@@ -36,7 +36,7 @@ class River:
         return None
     
     def view_river(self):
-        print(f"~~~ Day {self.day}: ~~~\nFish population: {self.fish}\nBear population: {self.bears}")
+        print(f"~~~ Day {self.day}: ~~~\nFish population: {len(self.fish)}\nBear population: {len(self.bears)}")
         return None
             
     def one_river_day(self):
